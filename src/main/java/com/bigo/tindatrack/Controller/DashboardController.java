5d3508a6c3a995932db74fe3205beeee81d77f20
froilan <johannefroilan@gmail.com>
2026-04-27 21:36:36 +0800

    Logic for Buttons in DASHBOARD AND LOGOUT IMPLEMENTATION

--- src/main/java/com/bigo/tindatrack/Controller/DashboardController.java
+++ src/main/java/com/bigo/tindatrack/Controller/DashboardController.java
@@ -1,13 +1,21 @@
 package com.bigo.tindatrack.Controller;
 
 import com.bigo.tindatrack.data.models.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
-import java.time.format.TextStyle;
 
 
 import static com.bigo.tindatrack.SQLite_Database.userManagement.SessionManager.loadUser;
@@ -17,7 +25,8 @@ public class DashboardController {
 
     @FXML
     private Label welcomeField, username_top, username_bottom, dateField;
-
+    @FXML
+    private Button inventoryButton, insightButton, stockactivityButton, settingButton, viewAllerts;
 
     private User user = loadUser();
 
@@ -39,5 +48,58 @@ public class DashboardController {
         dateField.setText(dayName + ", " + formatted + " - ");
 
     }
+
+    //helper for the switch screens
+    private void switchScene(ActionEvent event, String fxmlPath) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//            Stage stage = (Stage) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Error loading scene: " + fxmlPath);
+            e.printStackTrace();
+        }
+    }
+
+
+    // These are all the buttons for the dashboard and this includes the logout button that resets the SessionManager for user
+    @FXML
+    public void goToInventoryButton(ActionEvent event){
+                    switchScene(event,"/com/bigo/tindatrack/Inventory-view.fxml" );
+    }
+    @FXML
+    public void goToInsightButton(ActionEvent event ){
+                    switchScene(event, "/com/bigo/tindatrack/insight-view.fxml");
+    }
+    @FXML
+    public void goToStockactivityButton(ActionEvent event){
+                switchScene(event, "/com/bigo/tindatrack/Stocksactivity-view.fxml");
+    }
+    @FXML
+    public void goToSettingButton(ActionEvent event){
+        switchScene(event, "/com/bigo/tindatrack/Settings-view.fxml");
+    }
+    @FXML
+    public void gotoviewAllerts(ActionEvent event){
+        switchScene(event,"/com/bigo/tindatrack/Inventory-view.fxml" );
+    }
+    @FXML
+    public void goThreeDashboard(ActionEvent event){
+        switchScene(event, "/com/bigo/tindatrack/Dashboard-view.fxml");
+    }
+    @FXML
+    public void setLogout(ActionEvent event) {
+        com.bigo.tindatrack.SQLite_Database.userManagement.SessionManager.clearSession();
+        switchScene(event, "/com/bigo/tindatrack/Login-view.fxml");
+    }
+
+
+
+
+
+
+
 
 }
--- src/main/java/com/bigo/tindatrack/Controller/InventoryController.java
+++ src/main/java/com/bigo/tindatrack/Controller/InventoryController.java
@@ -1,5 +1,38 @@
 package com.bigo.tindatrack.Controller;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class InventoryController {
 
+
+
+
+    //this is helper function for switching screens
+    private void switchScene(ActionEvent event, String fxmlPath) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//            Stage stage = (Stage) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("Error loading scene: " + fxmlPath);
+            e.printStackTrace();
+        }
+    }
+
+    // these are function that switches screen from inventory more to add.
+    @FXML
+    public void setToDashboard(ActionEvent event){
+        switchScene(event,"/com/bigo/tindatrack/Dashboard-view.fxml");
+    }
 }
