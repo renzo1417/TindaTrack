1bf5b4bef82195c2ea0e9c1ab7d280f1c8c8774e
Kashkin <fridge862@gmail.com>
2026-04-27 21:19:42 +0800

    Implemeted PasswordHandler.java this class has 2 methods which are - verifyUser : in order to verify user before being able to change pass. This method acceps fullname, phoneNumber, storeName all in String format. Which the perimeters values are passed to a query then if all matches proceeds to return the matching username(why i didnt use id reason found in testArea)
    - changePass : this methods accepts username and newPassword in string format. Why does it accept username? it accepts username from the verifyUser
    
    process:
    verifyUser -> changePass
    verifyUser passes the matching username then changePass sends a query to the db to change the pass to the matching verified username.

--- src/main/java/com/bigo/tindatrack/SQLite_Database/testArea.java
+++ src/main/java/com/bigo/tindatrack/SQLite_Database/testArea.java
@@ -3,6 +3,8 @@ package com.bigo.tindatrack.SQLite_Database;
 import static com.bigo.tindatrack.SQLite_Database.TableManagement.createUserTable;
 import static com.bigo.tindatrack.SQLite_Database.userManagement.AuthenticateUser.authenticateUser;
 import static com.bigo.tindatrack.SQLite_Database.userManagement.CreateUser.createUser;
+import static com.bigo.tindatrack.SQLite_Database.userManagement.PasswordHandler.changePass;
+import static com.bigo.tindatrack.SQLite_Database.userManagement.PasswordHandler.verifyUser;
 
 public class testArea {
 
@@ -11,14 +13,27 @@ public class testArea {
         // 1username, 2fullname, 3password, 4email, 5phoneNumber, 6storeName
 
         createUserTable();
-        System.out.println("\nTEST REGS:");
-        createUser("test1", "test first", "123", "test@j.com", "011", "sari");
-        createUser("admin", "admin gwapo", "123", "test@k.com","022", "adminStore");
-        createUser("admin", "admin gwapo", "213", "test@k.com","022", "adminStore");
+//        System.out.println("\nTEST REGS:");
+//        createUser("test1", "testfirst", "123", "test@j.com", "011", "sari");
+//        createUser("admin", "admin gwapo", "123", "test@k.com","022", "adminStore");
+//        createUser("admin", "admin gwapo", "213", "test@k.com","022", "adminStore");
 
         System.out.println("\nLogin test: ");
-        authenticateUser("test1","123");
+        authenticateUser("test1","newlols");
         authenticateUser("admin","123");
+        System.out.println();
+
+        System.out.println("TEST CHANGEPASS");
+        verifyUser("test first", "011", "sari");
+        System.out.println(changePass(verifyUser("test first", "011", "sari"), "newlols"));
+        // why is the methods in a sout?
+        // ans:
+        // changePass returns a boolean function if it succeeded in changing the password
+        // why does verifyUser inside the changePass perimeter?
+        // verifyUser returns the username, was debating between id, username, and fullname
+        // dint use fullname because edgecase: there might be two same fullnames entries
+        // didnt use id because i wanted to use username lol since its unique but will change it later in case
+        // something comes up
 
     }
 
---
+++ src/main/java/com/bigo/tindatrack/SQLite_Database/userManagement/PasswordHandler.java
@@ -0,0 +1,70 @@
+package com.bigo.tindatrack.SQLite_Database.userManagement;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.bigo.tindatrack.SQLite_Database.TableManagement.connect;
+
+public class PasswordHandler {
+
+    String userForChange = null;
+
+
+    // first version of this was a basic boolean function where it will return true and false but i thought that it
+    // wouldnt work well
+
+    // i used username since it will be a unique value instead of fullname
+    //so I made the verifyUser return a username String
+    public static String verifyUser(String fullname, String phoneNumber, String storeName){
+        String query = "SELECT username FROM users WHERE fullname = ? AND phoneNumber = ? AND storeName = ?";
+        try(Connection connected = connect(); PreparedStatement pstmt = connected.prepareStatement(query)){
+
+            //for testing purps
+            System.out.println("fullname inputted: " + fullname);
+            System.out.println("phoneNumber inputted: " + phoneNumber);
+            System.out.println("storeName inputted: " + storeName);
+
+            pstmt.setString(1, fullname);
+            pstmt.setString(2, phoneNumber);
+            pstmt.setString(3, storeName);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next()){
+                String usernameFromDB = rs.getString("username");
+                System.out.println("User Verified : " + usernameFromDB);
+                return usernameFromDB;
+            }else{
+                System.err.println("NO MATCHING USER FOUND!");
+                return null;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("ERROR CONNECTING");
+            return null;
+        }
+
+    }
+
+    public static boolean changePass(String username, String newPassword){
+        String query = "UPDATE users SET password = ? WHERE username = ?";
+        try(Connection connected = connect(); PreparedStatement pstmt = connected.prepareStatement(query)){
+
+            pstmt.setString(1,newPassword);
+            pstmt.setString(2,username);
+
+            pstmt.executeUpdate();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.err.println("ERROR CONNECTING");
+            return false;
+        }
+
+    }
+
+}
+
